8a5730f30de8835a9b852b104c5f0550a404df6d
kkt6394 <kkt6394@gmail.com>
2026-01-26 22:13:50 +0900

    feat: NetworkManager 1차 구현

--- NetworkProjectDay20/Manager/NetworkManager.swift
+++ NetworkProjectDay20/Manager/NetworkManager.swift
@@ -12,5 +12,38 @@ class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
-    
+    func callRequest(completion: @escaping (Result<SearchData, AFError>) -> Void) {
+        
+        let url = "https://api.unsplash.com/search/photos"
+        let header: HTTPHeaders = [
+            "Accept-Version": "v1"
+        ]
+        let param: Parameters = [
+            "query": "query",
+            "page": 1,
+            "per_page": 20,
+            "order_by": "latest",
+            "color": "yellow",
+            "client_id": APIKey.key
+        ]
+        
+        AF.request(
+            url,
+            method: .get,
+            parameters: param,
+            encoding: URLEncoding.queryString,
+            headers: header
+        )
+        .validate(statusCode: 200..<300)
+        .responseDecodable(of: SearchData.self) {
+            response in
+            switch response.result {
+                
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
 }
