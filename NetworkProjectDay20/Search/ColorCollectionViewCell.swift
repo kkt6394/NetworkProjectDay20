52a113ec2a19de99e6189bc31696da3a7aa61137
kkt6394 <kkt6394@gmail.com>
2026-01-26 19:47:22 +0900

    feat: cell 구현

---
+++ NetworkProjectDay20/Search/ColorCollectionViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  ColorCollectionViewCell.swift
+//  NetworkProjectDay20
+//
+//  Created by 김기태 on 1/26/26.
+//
+
+import UIKit
+import SnapKit
+
+class ColorCollectionViewCell: UICollectionViewCell {
+    let button = {
+        let button = UIButton()
+        return button
+    }()
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        [
+            button
+        ].forEach { contentView.addSubview($0) }
+
+    }
+    
+    func configureLayout() {
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(80)
+            make.height.equalTo(40)
+        }
+    }
+    func configureView() {
+//        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .bold)
+//        let image = UIImage(systemName: "circle.fill", withConfiguration: symbolConfig)
+//        var config = UIButton.Configuration.plain()
+//        config.image = image
+//        config.title = "색깔"
+//        config.
+//        config.imagePadding = 4
+//        config.baseBackgroundColor = .systemGray5
+//        config.baseForegroundColor = .black
+//        config.imageColorTransformer = .init { _ in .systemYellow}
+//        button.configuration = config
+//        button.layer.cornerRadius = 10
+//        button.clipsToBounds = true
+
+    }
+    
+    func configureCell() {
+        button.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        button.setTitle("색깔", for: .normal)
+
+    }
+}
---
+++ NetworkProjectDay20/Search/SearchViewController.swift
@@ -0,0 +1,97 @@
+//
+//  SearchViewController.swift
+//  NetworkProjectDay20
+//
+//  Created by 김기태 on 1/26/26.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: BaseViewController {
+
+    let pageTitle = UILabel()
+    let searchBar = UISearchBar()
+    lazy var colorCollectionView = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+    }
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        [
+            pageTitle, searchBar, colorCollectionView
+        ].forEach { view.addSubview($0) }
+        
+    }
+    override func configureLayout() {
+        super.configureLayout()
+        pageTitle.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.height.equalTo(44)
+        }
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(pageTitle.snp.bottom)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(44)
+        }
+        colorCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(10)
+            make.height.equalTo(44)
+        }
+    }
+    override func configureView() {
+        super.configureView()
+        pageTitle.text = "SEARCH PHOTO"
+        pageTitle.font = .boldSystemFont(ofSize: 18)
+        pageTitle.textAlignment = .center
+        
+        searchBar.placeholder = "키워드 검색"
+        searchBar.searchTextField.backgroundColor = .systemGray5
+    }
+    
+    func configureCollectionView() {
+        colorCollectionView.register(
+            ColorCollectionViewCell.self,
+            forCellWithReuseIdentifier: String(
+                describing: ColorCollectionViewCell.self
+            )
+        )
+        colorCollectionView.delegate = self
+        colorCollectionView.dataSource = self
+        
+    }
+    
+    func setCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let width = UIScreen.main.bounds.width / 5
+        layout.itemSize = CGSize(width: width, height: 44)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+        layout.minimumInteritemSpacing = 10
+        
+        return layout
+    }
+
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ColorCollectionViewCell.self), for: indexPath) as? ColorCollectionViewCell else { return UICollectionViewCell() }
+        cell.configureCell()
+        return cell
+    }
+    
+    
+    
+    
+}
